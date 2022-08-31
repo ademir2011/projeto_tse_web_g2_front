ae2e88c2db6a0b5254e4b6bb6fe2c45f0dfffd31
Iramar Ferreira <iramarbsi@gmail.com>
2022-08-31 00:14:34 -0300

    Tela de Login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,11 @@ export default function Header() {
       <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between">
           <div className="flex w-0 flex-1 items-center">
-            <span className="flex rounded-lg bg-indigo-800 p-2">
-              <MegaphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            <span className="flex rounded-lg bg-indigo-800 p-2 hover:bg-indigo-700">
+              
+              <MegaphoneIcon className="h-6 w-6 text-white" aria-hidden="true" onClick={() => {
+                location.href="/"
+              }}  />
             </span>
             <p className="text-2xl ml-3 truncate font-medium text-white">
               <span className="md:hidden">Sistema de Controle de Pautas</span>
@@ -17,7 +20,7 @@ export default function Header() {
           </div>
           <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
             <a
-              href="#"
+              href="/login"
               className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-lg font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
             >
               Login
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import './global.css'
+// import { BrowserRouter } from "react-router-dom";
+import RoutesConfig from './RoutesConfig';
+import Login from './pages/Login';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App />}/>
+        <Route index element={<App />} />
+        <Route path="/login" element={<Login />} />
+
+    </Routes>
+</BrowserRouter>
 )
